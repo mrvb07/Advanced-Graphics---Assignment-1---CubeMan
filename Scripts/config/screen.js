36223c99811aacd29003f1283e8e722fd93ac4b2
Vinay Bhardwaj <Vinay Bhardwaj>
2016-02-01 14:40:31 -0500

    added cube

---
+++ Scripts/config/screen.js
@@ -0,0 +1,13 @@
+var config;
+(function (config) {
+    var Screen = (function () {
+        function Screen() {
+        }
+        Screen.WIDTH = 1024;
+        Screen.HEIGHT = 720;
+        Screen.RATIO = 1.333333;
+        return Screen;
+    })();
+    config.Screen = Screen;
+})(config || (config = {}));
+//# sourceMappingURL=screen.js.map
---
+++ Scripts/config/screen.ts
@@ -0,0 +1,8 @@
+module config {
+    export class Screen {
+        static WIDTH:number = 1024;
+        static HEIGHT:number = 720;
+        static RATIO:number = 1.333333;
+    }
+    
+}
